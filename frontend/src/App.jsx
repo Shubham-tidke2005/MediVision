8577904eac9f097c3c91ceae5f1cbcc86d3d7f68
Shubham Tidke <tidkeshubham826@gmail.com>
2026-09-12 17:57:23 +0530

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
+
+//shuru karte he 
 function App() {
   const [count, setCount] = useState(0)
 
